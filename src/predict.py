47969306ba89b72bab95bae2926b45d2b6057d6e
marcus <mhagglun@gmail.com>
2020-05-10 21:02:27 +0200

    add plots for training and result

--- src/predict.py
+++ src/predict.py
@@ -1,14 +1,16 @@
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
 import matplotlib.pyplot as plt
 import numpy as np
 import model
 import preprocessing
 
 
-
 dataset = preprocessing.Dataset().load_batch('data/images')
 
-train_images = dataset['image'][0:2, :, :]
-train_masks = dataset['mask']
+N = 10
+train_images = dataset['image'][0:N, :, :]
+train_masks = dataset['mask'][0:N, :, :]
 
 train_images = np.expand_dims(train_images, axis=3)
 train_masks = np.expand_dims(train_masks, axis=3)
@@ -21,13 +23,22 @@ model.load_weights('weights')
 
 predictions = model.predict(train_images, batch_size=1, verbose=1)
 
-ground_truth_mask = dataset['mask'][0, :, :]
-predicted_mask = predictions[0, :, :]
+for n in range(N):
+    true_mask = dataset['mask'][n, :, :].reshape(512, 512)
+    predicted_mask = predictions[n, :, :].reshape(512, 512)
 
-plt.imshow(predicted_mask.reshape(512, 512),
-                    cmap='gray', interpolation='none')
-plt.show()
+    predicted_mask = (predicted_mask - np.min(predicted_mask)) / \
+        (np.max(predicted_mask) - np.min(predicted_mask))
+    predicted_mask[predicted_mask > 0.5] = 1
+    predicted_mask[predicted_mask <= 0.5] = 0
 
-preprocessing.display_image(train_images[0, :,:], predicted_mask)
+    preprocessing.display_image(train_images[n, :, :], predicted_mask)
 
-preprocessing.display_image(train_images[0, :,:], ground_truth_mask)
+    preprocessing.display_image(train_images[n, :, :], true_mask)
+
+
+def dice_loss(y_true, y_pred):
+    num = 2.0 * tf.reduce_sum(y_true * y_pred, axis=-1)
+    denom = tf.reduce_sum(y_true + y_pred, axis=-1)
+    return 1.0 - (num + 1.0) / (denom + 1.0)
+
--- src/train.py
+++ src/train.py
@@ -1,30 +1,55 @@
+import tensorflow as tf
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 import model
 import preprocessing
+
+sns.set_style('darkgrid')
+# tf.config.optimizer.set_jit(True)
+# tf.config.set_soft_device_placement(True)
+# tf.debugging.set_log_device_placement(True)
 
 dataset = preprocessing.Dataset().load_batch('data/images')
 
 train_images = dataset['image']
 train_masks = dataset['mask']
-
 train_images = np.expand_dims(train_images, axis=3)
 train_masks = np.expand_dims(train_masks, axis=3)
 
-
 batch_size = 1
-epochs = 1
+epochs = 10
 
 model = model.U_net((512, 512, 1), n_filters=32, dropout=None)
-model.summary()
+# model.summary()
+# tf.keras.utils.plot_model(U_net(), to_file='model.png',show_shapes=True)
 
 # Checkpoint for storing weights during learning
 checkpoint = ModelCheckpoint(
     'weights', save_best_only=True, monitor='val_loss', mode='min')
 
 # Reduces learning rate when a metric has stopped improving
-reduce_lr_loss = ReduceLROnPlateau(
-    monitor='val_loss', factor=0.1, patience=2, verbose=1, epsilon=1e-4, mode='min')
+reduce_lr = ReduceLROnPlateau(
+    monitor='val_loss', factor=0.8, patience=2, verbose=1, min_lr=1e-4, mode='min')
 
-result = model.fit(train_images, train_masks, batch_size=batch_size, epochs=epochs, shuffle=True, verbose=1, validation_split=0.1,
-                   callbacks=[checkpoint, reduce_lr_loss])
+result = model.fit(train_images, train_masks, batch_size=batch_size, epochs=epochs, shuffle=True, verbose=1, validation_split=0.2,
+                   callbacks=[checkpoint, reduce_lr])
+
+# plt.figure(figsize=(12.0, 5.0))
+# plt.subplot(1, 2, 1, label='accuracy plot')
+# plt.plot(result.history['accuracy'])
+# plt.plot(result.history['val_accuracy'])
+# plt.title('model accuracy')
+# plt.ylabel('accuracy')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'val'], loc='upper left')
+
+# plt.subplot(1, 2, 2, label='loss plot')
+plt.plot(result.history['loss'])
+plt.plot(result.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.show()
